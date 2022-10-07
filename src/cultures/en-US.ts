4f2798b81fd2e481684cecfc7b0ec592a44c7a6c
Jacques L. Chereau <jlchereau@users.noreply.github.com>
2022-10-07 16:57:41 +0200

    Added culture

---
+++ src/cultures/en-US.ts
@@ -0,0 +1,62 @@
+export const culture = {
+    name: 'en-US',
+    numberFormat: {
+        pattern: ['-n'],
+        decimals: 2,
+        ',': ',',
+        '.': '.',
+        groupSize: [3],
+        percent: {
+            pattern: ['-n %', 'n %'],
+            decimals: 2,
+            ',': ',',
+            '.': '.',
+            groupSize: [3],
+            symbol: '%'
+        },
+        currency: {
+            name: 'US Dollar',
+            abbr: 'USD',
+            pattern: ['($n)', '$n'],
+            decimals: 2,
+            ',': ',',
+            '.': '.',
+            groupSize: [3],
+            symbol: '$'
+        }
+    },
+    calendars: {
+        standard: {
+            days: {
+                names: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+                namesAbbr: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                namesShort: [ 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa' ]
+            },
+            months: {
+                names: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                namesAbbr: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            AM: [ 'AM', 'am', 'AM' ],
+            PM: [ 'PM', 'pm', 'PM' ],
+            patterns: {
+                d: 'M/d/yyyy',
+                D: 'dddd, MMMM dd, yyyy',
+                F: 'dddd, MMMM dd, yyyy h:mm:ss tt',
+                g: 'M/d/yyyy h:mm tt',
+                G: 'M/d/yyyy h:mm:ss tt',
+                m: 'MMMM dd',
+                M: 'MMMM dd',
+                // s: "yyyy'-'MM'-'ddTHH':'mm':'ss",
+                t: 'h:mm tt',
+                T: 'h:mm:ss tt',
+                // u: "yyyy'-'MM'-'dd HH':'mm':'ss'Z'",
+                y: 'MMMM, yyyy',
+                Y: 'MMMM, yyyy'
+            },
+            '/': '/',
+            ':': ':',
+            firstDay: 0,
+            twoDigitYearMax: 2029
+        }
+    }
+};
