65bcf0fbb5e83938e2e9dce6d9ad1ae17458f826
jlchereau <jlchereau@msn.com>
2022-10-19 18:06:08 +0200

    Made data optional

--- src/error/assert.ts
+++ src/error/assert.ts
@@ -1,7 +1,12 @@
 import OpenXMLError from './OpenXMLError';
 
+/**
+ * assert
+ * @param condition
+ * @param message
+ */
 export function assert(condition: any, message?: string): asserts condition {
     if (!condition) {
-        throw new OpenXMLError(1001, { data: { message } });
+        throw new OpenXMLError(1001, message ? { data: { message } } : undefined);
     }
 }
