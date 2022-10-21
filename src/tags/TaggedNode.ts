94d69b085a42d1a9e4f31bcac4126443f41416b2
jlchereau <jlchereau@msn.com>
2022-10-21 20:01:20 +0200

    Made match public

--- src/tags/TaggedNode.ts
+++ src/tags/TaggedNode.ts
@@ -1,24 +1,57 @@
+import constants from "../constants";
+
 /**
  * TaggedNode
  */
 class TaggedNode {
+    // TODO consider making node and match private
     public node: Text;
-    private _match: Array<string>;
+    public match: Array<string>;
+
+    /**
+     * constructor
+     * @param node
+     * @param match
+     */
     constructor(node: Text, match: Array<string>) {
         this.node = node;
-        this._match = match;
+        this.match = match;
     }
 
+    /**
+     * statement
+     */
     get statement() {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        return this._match.groups.statement;
+        return this.match.groups.statement;
     }
 
+    /**
+     * expression
+     */
     get expression() {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        return this._match.groups.expression;
+        return this.match.groups.expression;
+    }
+
+    /**
+     * nodeValue
+     */
+    get nodeValue(): string | null {
+        return this.node.nodeValue;
+    }
+
+    /**
+     * replaceTag
+     * @param data
+     */
+    replaceTag(data: string) {
+        const tag: string = this.match[0];
+        const offset: number = (this.node.nodeValue || constants.empty).indexOf(tag);
+        const count = tag.length;
+        return this.node.replaceData(offset, count, data);
     }
 }
 
