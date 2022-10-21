497bcbe0180cdee4fe969855b6f23b9bf4bcd006
jlchereau <jlchereau@msn.com>
2022-10-21 19:52:44 +0200

    Added configurable expression engine

--- src/tags/ExpressionTag.ts
+++ src/tags/ExpressionTag.ts
@@ -1,33 +1,35 @@
 import AbstractTag from "./AbstractTag";
 import ITag from "./ITag";
 import TaggedNode from "./TaggedNode";
+import expressionEngine from "./expressionEngine";
+import constants from "../constants";
+import {assert} from "../error/assert";
 
 /**
  * ExpressionTag
  */
 class ExpressionTag extends AbstractTag implements ITag {
-    static readonly tag = '';
-    static readonly statements: Array<string> = [''];
+    static readonly statement = constants.empty;
+    static readonly blocks: Array<string> = [];
 
     /**
      * constructor
      * @param node
+     * @param parent
      */
-    constructor(node: TaggedNode) {
-        super(node);
+    constructor(node: TaggedNode, parent: Array<AbstractTag>) {
+        super(node, parent);
     }
 
     /**
      * render
      * @param data
      */
-    async render(data: Record<string, unknown> = {}) {
-        // Merge with data
-        const node = this.nodes.get(ExpressionTag.statements[0])?.node;
-        if (node) {
-            // const template = handlebars.compile(node.nodeValue);
-            // node.replaceData(0, (node.nodeValue || '').length, template(data));
-        }
+    async render(data: Record<string, unknown>) {
+        const taggedNode = this.nodes.get(ExpressionTag.statement);
+        assert(taggedNode instanceof TaggedNode);
+        const str: string = <string>await expressionEngine.evaluate(taggedNode.expression, data);
+        taggedNode.replaceTag(str);
         this._done = true;
     }
 }
