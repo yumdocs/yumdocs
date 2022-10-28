5c813469382fe573f8efb95fbbabf74d3cad09e1
jlchereau <jlchereau@msn.com>
2022-10-28 09:11:44 +0200

    Added most common cultures

--- src/cultures/cultureMap.ts
+++ src/cultures/cultureMap.ts
@@ -1,15 +1,35 @@
+import arEG from "./ar-EG";
+import bnIN from "./bn-IN";
+import deDE from "./de-DE";
 import enGB from "./en-GB";
 import enUS from "./en-US";
+import esES from "./es-ES";
 import frFR from "./fr-FR";
+import hiIN from "./hi-IN";
+import itIT from "./it-IT";
+import jaJP from "./ja-JP";
+import ptPT from "./pt-PT";
+import ruRU from "./ru-RU";
+import zhCN from "./zh-CN";
 
 /**
  * cultureMap
  * Note: avoids circular references with YumTemplate
  */
 const cultureMap = new Map<string, Record<string, unknown>>([
+    ['ar-EG', arEG],
+    ['bn-IN', bnIN],
+    ['de-DE', deDE],
     ['en-GB', enGB],
     ['en-US', enUS],
-    ['fr-FR', frFR]
+    ['es-ES', esES],
+    ['fr-FR', frFR],
+    ['hi-IN', hiIN],
+    ['it-IT', itIT],
+    ['ja-JP', jaJP],
+    ['pt-PT', ptPT],
+    ['ru-RU', ruRU],
+    ['zh-CN', zhCN],
 ]);
 
 /**
