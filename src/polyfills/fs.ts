97e3708c79d294a88ceefbce3da508ff4a7ec3bc
jlchereau <jlchereau@msn.com>
2022-10-31 23:33:50 +0100

    Attempt at resolving loading 'fs' error in docusaurus

--- src/polyfills/fs.ts
+++ src/polyfills/fs.ts
@@ -2,10 +2,14 @@ import {isNodeJS} from './polyfillsUtils';
 import YumError from "../error/YumError";
 
 const error = () => { throw new YumError(1020); } // TODO Review code
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export const promises = isNodeJS ? require('fs').promises : {
-    mkdir: error,
-    readFile: error,
-    writeFile: error
+const req = isNodeJS ? require : () => {
+    return {
+        promises : {
+            mkdir: error,
+            readFile: error,
+            writeFile: error
+        }
+    };
 };
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const { promises } = req('fs');
